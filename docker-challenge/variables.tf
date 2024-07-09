1d2f20d6b186a9bad425ce6c4d3d82b445a9e037
godofdreamers <pda@lucidnight.com>
2024-07-09 18:23:25 +0000

    docker challenge

---
+++ docker-challenge/variables.tf
@@ -0,0 +1,15 @@
+variable "container_name" {
+  type = string
+  default = "Alta3ResearchWebService"
+}
+
+variable "internal_port" {
+  type = number
+  default = 9876
+}
+
+variable "external_port" {
+  type = number
+  default = 5432
+}
+
